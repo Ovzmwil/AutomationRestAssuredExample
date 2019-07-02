e6842bf1e9af5f589861f14326f6971c9be46ce5
Leandro Santana <leandro.rodrigues.santana@hotmail.com>
2019-07-02 14:42:23 -0300

    :art: Try e catch ao pegar número de serviços

--- src/test/java/com/leandro/restassured/example/test/FirstTest.java
+++ src/test/java/com/leandro/restassured/example/test/FirstTest.java
@@ -3,6 +3,7 @@ package com.leandro.restassured.example.test;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -16,45 +17,51 @@ import io.restassured.response.Response;
 
 public class FirstTest extends GenerateReport {
 
-    Response response;
-    
-    @BeforeMethod
-    public void set() {
-    	RestAssured.baseURI = URI.getRiotURI();
-    }
-    
-    @Test
-    public void freeWeekTestParamKey() {
-    	response = given().param("api_key", Keys.getApikey()).when().get("/lol/platform/v3/champion-rotations");
-    	
-    	assertThat(response.getStatusCode(), equalTo(200));
-    	assertThat(response.jsonPath().getInt("freeChampionIds.size()"), equalTo(14));
-    	assertThat(response.jsonPath().getInt("freeChampionIdsForNewPlayers.size()"), equalTo(10));
-    	assertThat(response.jsonPath().getInt("maxNewPlayerLevel"), equalTo(10));
-    }
-    
-    @Test
-    public void freeWeekTestHeaderKey() {
-    	response = given().header("X-Riot-Token", Keys.getApikey()).when().get("/lol/platform/v3/champion-rotations");
-    	
-    	assertThat(response.getStatusCode(), equalTo(200));
-    	assertThat(response.jsonPath().getInt("freeChampionIds.size()"), equalTo(14));
-    	assertThat(response.jsonPath().getInt("freeChampionIdsForNewPlayers.size()"), equalTo(10));
-    	assertThat(response.jsonPath().getInt("maxNewPlayerLevel"), equalTo(10));
-    }
-    
-    @Test
-    public void serverStatusTest() {
-    	response = given().header("X-Riot-Token", Keys.getApikey()).when().get("/lol/status/v3/shard-data");
-    	
-    	int servicesListSize = response.jsonPath().getInt("services.size()");
-    	
-    	assertThat(response.getStatusCode(), equalTo(200));
-    	assertThat(response.jsonPath().getString("name"), equalTo("Brazil"));
-    	
-    	for (int i = 0; i < servicesListSize; i++) {
-    		assertThat(response.jsonPath().getString("services[" + i + "].status"), equalTo("online"));
-    	}
-    }
-    
+	Response response;
+
+	@BeforeMethod
+	public void set() {
+		RestAssured.baseURI = URI.getRiotURI();
+	}
+
+	@Test
+	public void freeWeekTestParamKey() {
+		response = given().param("api_key", Keys.getApikey()).when().get("/lol/platform/v3/champion-rotations");
+
+		assertThat(response.getStatusCode(), equalTo(200));
+		assertThat(response.jsonPath().getInt("freeChampionIds.size()"), equalTo(14));
+		assertThat(response.jsonPath().getInt("freeChampionIdsForNewPlayers.size()"), equalTo(10));
+		assertThat(response.jsonPath().getInt("maxNewPlayerLevel"), equalTo(10));
+	}
+
+	@Test
+	public void freeWeekTestHeaderKey() {
+		response = given().header("X-Riot-Token", Keys.getApikey()).when().get("/lol/platform/v3/champion-rotations");
+
+		assertThat(response.getStatusCode(), equalTo(200));
+		assertThat(response.jsonPath().getInt("freeChampionIds.size()"), equalTo(14));
+		assertThat(response.jsonPath().getInt("freeChampionIdsForNewPlayers.size()"), equalTo(10));
+		assertThat(response.jsonPath().getInt("maxNewPlayerLevel"), equalTo(10));
+	}
+
+	@Test
+	public void serverStatusTest() {
+		response = given().header("X-Riot-Token", Keys.getApikey()).when().get("/lol/status/v3/shard-data");
+		int servicesListSize = 0 ;
+
+		try {
+			servicesListSize = response.jsonPath().getInt("services.size()");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		assertThat(response.getStatusCode(), equalTo(200));
+		assertThat(response.jsonPath().getString("name"), equalTo("Brazil"));
+		assertThat(servicesListSize, not(equalTo(0)));
+
+		for (int i = 0; i < servicesListSize; i++) {
+			assertThat(response.jsonPath().getString("services[" + i + "].status"), equalTo("online"));
+		}
+	}
+
 }
